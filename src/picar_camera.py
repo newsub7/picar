9a192ca6a65c9b14d9731907c9e4e9ed53fffa02
Max Huebner <maximilianhuebner@gmx.net>
2022-01-30 22:52:54 +0000

    Lib hinzugefügt

--- src/picar_camera.py
+++ src/picar_camera.py
@@ -1,11 +1,23 @@
+#!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
 import cv2
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+#import picarx from Picarx
+
+def talker():
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    rospy.init_node('pi_camera_node')
+    rate = rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+        hello_str = "Hello world %s" % rospy.get_time()
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('pi_camera_node')
-    
-    
-    
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
